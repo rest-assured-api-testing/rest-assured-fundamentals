19e0f6d9a200dc13592a764fe0e1f62cf7fcdeda
marylin moya <marylin.moya@jalasoft.com>
2021-06-10 15:49:31 -0400

    Rest Assured features

---
+++ src/test/java/ResponseSpecification3Test.java
@@ -0,0 +1,33 @@
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.ResponseSpecification;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class ResponseSpecification3Test {
+
+    private ResponseSpecification responseSpecification;
+
+    @BeforeClass
+    public void createResponseSpecification() {
+        responseSpecification = new ResponseSpecBuilder()
+                .expectContentType(ContentType.JSON)
+                .expectStatusCode(200)
+                .build();
+    }
+
+    @Test
+    public void test2() {
+        given()
+                .when()
+                .get("http://zippopotam.us/us/90210")
+                .then()
+                .spec(responseSpecification)
+                .assertThat()
+                .log().all()
+                .body("places[0].'place name'", equalTo("Beverly Hills"));
+    }
+}
