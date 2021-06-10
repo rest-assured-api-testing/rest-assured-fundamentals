37689360d35cbc3484221c2610ee695f3ebaa4e6
Juan Pablo Gonzales <juampi7237@gmail.com>
2021-06-10 19:39:08 -0400

    Junit with DataProvider

---
+++ src/test/java/juangonzales_dataprovider/ParameterizationTest.java
@@ -0,0 +1,42 @@
+package juangonzales_dataprovider;
+
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+
+@RunWith(DataProviderRunner.class)
+public class ParameterizationTest {
+
+
+    @DataProvider
+    public static Object[][] dataProviderAdd() {
+        return  new Object[][]{
+                {"us", "90210", "Beverly Hills"},
+                {"us", "12345", "Schenectady"},
+                {"ca", "B2R", "Waverley"},
+                {"nl", "1001", "Amsterdam"}
+        };
+    }
+
+    @Test
+    @UseDataProvider("dataProviderAdd")
+    public void testAdd(String countryCode, String zipCode, String expectedResult)  {
+        given()
+                .pathParam("countryCode", countryCode)
+                .pathParam("zipCode", zipCode)
+                .when()
+                .get("http://zippopotam.us/{countryCode}/{zipCode}")
+                .then()
+                .assertThat()
+                .body("places[0].'place name'", equalTo(expectedResult));
+    }
+}
+
---
+++ src/test/java/juangonzales_dataprovider/ResponseExtracion5Test.java
@@ -0,0 +1,36 @@
+package juangonzales_dataprovider;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class ResponseExtracion5Test {
+
+    private RequestSpecification requestSpecification;
+
+    @BeforeClass
+    public void createRequestSpecification() {
+        requestSpecification = new RequestSpecBuilder()
+                .setBaseUri("http://zippopotam.us/")
+                .build();
+    }
+
+    @Test
+    public void test2() {
+        String placeName = given()
+                .spec(requestSpecification)
+                .when()
+                .get("us/90210")
+                .then()
+                .log().all()
+                .extract()
+                .path("places[0].'place name'");
+
+        Assert.assertEquals(placeName, "Beverly Hills");
+
+    }
+}
