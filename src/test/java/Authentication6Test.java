cf262c3c1c2cf48ee7d893cefc5c6e198698b88c
cristianfchq <cristian.fchq@gmail.com>
2021-06-10 20:44:06 -0400

    add parameterizacion to API todoist

--- src/test/java/Authentication6Test.java
+++ src/test/java/Authentication6Test.java
@@ -1,25 +1,25 @@
-import io.restassured.RestAssured;
-import io.restassured.specification.RequestSpecification;
-import org.apache.http.HttpStatus;
-import org.testng.annotations.Test;
-
-import static io.restassured.RestAssured.baseURI;
-
-public class Authentication6Test {
-
-    @Test
-    public void testLogin() {
-        baseURI = "https://api.todoist.com/rest/v1/projects";
-
-        RequestSpecification requestSpecification = RestAssured.given();
-
-        requestSpecification
-                .auth()
-                .oauth2("5703275f22fce7ac417a198be65764263fd0bf6b")
-                .when()
-                .get()
-                .then()
-                .statusCode(HttpStatus.SC_OK);
-    }
-
-}
+//import io.restassured.RestAssured;
+//import io.restassured.specification.RequestSpecification;
+//import org.apache.http.HttpStatus;
+//import org.testng.annotations.Test;
+//
+//import static io.restassured.RestAssured.baseURI;
+//
+//public class Authentication6Test {
+//
+//    @Test
+//    public void testLogin() {
+//        baseURI = "https://api.todoist.com/rest/v1/projects";
+//
+//        RequestSpecification requestSpecification = RestAssured.given();
+//
+//        requestSpecification
+//                .auth()
+//                .oauth2("5703275f22fce7ac417a198be65764263fd0bf6b")
+//                .when()
+//                .get()
+//                .then()
+//                .statusCode(HttpStatus.SC_OK);
+//    }
+//
+//}
